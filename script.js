1b9b01c7f7c62a4638da6bb599f8131edddedd9e
sangwanpriya021 <sangwanpriya021@gmail.com>
2026-07-23 22:50:34 +0530

    Updated script page

--- script.js
+++ script.js
@@ -60,3 +60,21 @@ function startQuiz() {
     window.location.href = "login.html";
 
 }
+document.getElementById("registerForm").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const response = await fetch("http://127.0.0.1:5000/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value
+        })
+    });
+
+    const result = await response.json();
+    alert(result.message);
+});
